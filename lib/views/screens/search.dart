52fa23f9106900217d1d2d64383871249f56982a
ITACHI <berdinazarovmraziz@gmail.com>
2025-05-25 06:49:27 +0500

    BMW

--- lib/views/screens/search.dart
+++ lib/views/screens/search.dart
@@ -1,3 +1,8 @@
+import 'package:exam_4_oy/views/widgets/dialog.dart';
+import 'package:exam_4_oy/views/widgets/food_card.dart';
+import 'package:exam_4_oy/views/widgets/keyword_chip.dart';
+import 'package:exam_4_oy/views/widgets/restorant_item.dart';
+import 'package:exam_4_oy/views/widgets/search.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -19,7 +24,6 @@ class SearchScreenState extends State<SearchScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            // Header qismi
             Row(
               children: [
                 IconButton(
@@ -51,12 +55,8 @@ class SearchScreenState extends State<SearchScreen> {
               ],
             ),
             SizedBox(height: 25),
-
-            // Search input qismi
             SearchInputField(controller: _searchController),
             SizedBox(height: 30),
-
-            // Recent Keywords
             Text(
               "Recent Keywords",
               style: TextStyle(
@@ -81,8 +81,6 @@ class SearchScreenState extends State<SearchScreen> {
               ),
             ),
             SizedBox(height: 30),
-
-            // Suggested Restaurants
             Text(
               "Suggested Restaurants",
               style: TextStyle(
@@ -99,8 +97,6 @@ class SearchScreenState extends State<SearchScreen> {
             SizedBox(height: 15),
             RestaurantItem(name: "Cafenia Coffee Club", rating: 4.0),
             SizedBox(height: 30),
-
-            // Popular Fast Food
             Text(
               "Popular Fast Food",
               style: TextStyle(
@@ -138,277 +134,5 @@ class SearchScreenState extends State<SearchScreen> {
   void dispose() {
     _searchController.dispose();
     super.dispose();
-  }
-}
-
-// Search Input Widget
-class SearchInputField extends StatelessWidget {
-  final TextEditingController controller;
-
-  const SearchInputField({super.key, required this.controller});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 55,
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade200,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          Icon(Icons.search, color: Colors.grey),
-          SizedBox(width: 10),
-          Expanded(
-            child: TextField(
-              controller: controller,
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Pizza",
-                hintStyle: TextStyle(fontSize: 16, color: Colors.grey),
-              ),
-            ),
-          ),
-          GestureDetector(
-            onTap: () {
-              controller.clear();
-            },
-            child: Container(
-              width: 30,
-              height: 30,
-              decoration: BoxDecoration(
-                color: Colors.grey.shade400,
-                shape: BoxShape.circle,
-              ),
-              child: Icon(Icons.close, color: Colors.white, size: 20),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// Keyword Chip Widget
-class KeywordChip extends StatelessWidget {
-  final String text;
-
-  const KeywordChip({super.key, required this.text});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      decoration: BoxDecoration(
-        color: Colors.grey.shade200,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Text(text, style: TextStyle(fontSize: 14, color: Colors.black)),
-    );
-  }
-}
-
-// Restaurant Item Widget
-class RestaurantItem extends StatelessWidget {
-  final String name;
-  final double rating;
-
-  const RestaurantItem({super.key, required this.name, required this.rating});
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade400,
-            borderRadius: BorderRadius.circular(12),
-          ),
-        ),
-        SizedBox(width: 15),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                ),
-              ),
-              SizedBox(height: 5),
-              Row(
-                children: [
-                  Icon(Icons.star, color: Colors.orange, size: 18),
-                  SizedBox(width: 5),
-                  Text(
-                    rating.toString(),
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.orange,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-// Food Card Widget
-class FoodCard extends StatelessWidget {
-  final String title;
-  final String subtitle;
-
-  const FoodCard({super.key, required this.title, required this.subtitle});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        boxShadow: [
-          BoxShadow(color: Colors.black12, blurRadius: 4, offset: Offset(0, 2)),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 120,
-            decoration: BoxDecoration(
-              color: Colors.grey.shade400,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                SizedBox(height: 5),
-                Text(
-                  subtitle,
-                  style: TextStyle(fontSize: 12, color: Colors.grey.shade600),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-// Offer Dialog Widget
-class OfferDialog extends StatelessWidget {
-  const OfferDialog({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Dialog(
-          backgroundColor: Colors.orange,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: SizedBox(
-            width: double.infinity,
-            height: 500,
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 100,
-                  left: 20,
-                  right: 20,
-                  bottom: 20,
-                ),
-                child: Column(
-                  children: [
-                    Text(
-                      "Hurry Offers!",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 45,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 60),
-                    Text(
-                      "#1243CD2",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    Text(
-                      "Use the cupon get 25% discount",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 30),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.orange,
-                        minimumSize: const Size(double.infinity, 75),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        side: BorderSide(color: Colors.white, width: 3),
-                      ),
-                      child: Text(
-                        "GOT IT",
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-          right: 30,
-          top: 142,
-          child: IconButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-            icon: Icon(
-              Icons.highlight_remove,
-              size: 65,
-              color: Colors.deepOrange,
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
